a88a0d1b53e4e26637ea5b6e54fb31cf48710861
treborsky <rob.koziarski2k@gmail.com>
2023-11-16 11:29:21 +0100

    implemented some vision algo for orange markers

--- include/mecanum_controller.hpp
+++ include/mecanum_controller.hpp
@@ -2,34 +2,34 @@
 #define MECANUM_CONTROLLER_HPP_
 
 #include <ros/ros.h>
-#include <sensor_msgs/Range.h>
+// #include <sensor_msgs/Range.h>
 #include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Image.h>
 
 #include <vector>
 
 class MecanumController {
+  static constexpr int H_MIN_ORANGE{0}, S_MIN_ORANGE{100}, V_MIN_ORANGE{50};
+  static constexpr int H_MAX_ORANGE{30}, S_MAX_ORANGE{255}, V_MAX_ORANGE{255};
+
+  static constexpr int L_MIN_ORANGE{25}, A_MIN_ORANGE{150}, B_MIN_ORANGE{150};
+  static constexpr int L_MAX_ORANGE{255}, A_MAX_ORANGE{230}, B_MAX_ORANGE{230};
+
   // node handle
   ros::NodeHandle node;
 
   // sub
-  ros::Subscriber ultraFrontSub;
-  ros::Subscriber ultraLeftSub;
-  ros::Subscriber ultraRightSub;
-  ros::Subscriber ultraBackSub;
   ros::Subscriber odomSub;
   ros::Subscriber depthSub;
   ros::Subscriber colorSub;
 
   // pub, timing
   // cmd_vel
+  ros::Publisher detPub;
   ros::Publisher cmdVelPub;
   ros::Rate cmdVelPubRate;
 
   // data & stuff -> updated a lot
-  double frontDistanceMeters;
-  double leftDistanceMeters;
-  double rightDistanceMeters;
-  double rearDistanceMeters;
   double posX;
   double posY;
 
@@ -47,11 +47,8 @@ class MecanumController {
   bool GetTargetAchieved() const { return targetAchieved; }
 
  private:
-  void UltraFrontCb(const sensor_msgs::Range::ConstPtr& msg);
-  void UltraLeftCb(const sensor_msgs::Range::ConstPtr& msg);
-  void UltraRightCb(const sensor_msgs::Range::ConstPtr& msg);
-  void UltraRearCb(const sensor_msgs::Range::ConstPtr& msg);
   void OdomCb(const nav_msgs::Odometry::ConstPtr& msg);
+  void ColorImgCb(const sensor_msgs::ImagePtr& msg);
 };
 
 #endif // MECANUM_CONTROLLER_HPP_
--- src/mecanum_controller.cpp
+++ src/mecanum_controller.cpp
@@ -2,22 +2,20 @@
 
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Range.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
 
 #include <utility>
+#include <vector>
 
 MecanumController::MecanumController(ros::NodeHandle nodeHandle) 
   : node(std::move(nodeHandle)),
-    ultraFrontSub(node.subscribe("/front_sensor", 5, &MecanumController::UltraFrontCb, this)),
-    ultraLeftSub(node.subscribe("/left_sensor", 5, &MecanumController::UltraFrontCb, this)),
-    ultraRightSub(node.subscribe("/right_sensor", 5, &MecanumController::UltraFrontCb, this)),
-    ultraBackSub(node.subscribe("/rear_sensor", 5, &MecanumController::UltraFrontCb, this)),
     odomSub(node.subscribe("/odom", 5, &MecanumController::OdomCb, this)),
+    colorSub(node.subscribe("/device_0/sensor_1/Color_0/image/data", 5, &MecanumController::ColorImgCb, this)),
+    detPub(node.advertise<sensor_msgs::Image>("/color/detection", 5, false)),
     cmdVelPub(node.advertise<geometry_msgs::Twist>("/cmd_vel", 5, false)),
     cmdVelPubRate(10),
-    frontDistanceMeters{5.0},
-    leftDistanceMeters{5.0},
-    rightDistanceMeters{5.0},
-    rearDistanceMeters{5.0},
     posX{0.0},
     posY{0.0} {}
 
@@ -65,23 +63,53 @@ void MecanumController::SetTarget(const double x, const double y) {
   targetAchieved = false;
 }
 
-void MecanumController::UltraFrontCb(const sensor_msgs::Range::ConstPtr& msg) {
-  frontDistanceMeters = msg->range;
-}
-
-void MecanumController::UltraLeftCb(const sensor_msgs::Range::ConstPtr& msg) {
-  leftDistanceMeters = msg->range;
-}
-
-void MecanumController::UltraRightCb(const sensor_msgs::Range::ConstPtr& msg) {
-  rightDistanceMeters = msg->range;
-}
-
-void MecanumController::UltraRearCb(const sensor_msgs::Range::ConstPtr& msg) {
-  rearDistanceMeters = msg->range;
-}
-
 void MecanumController::OdomCb(const nav_msgs::Odometry::ConstPtr& msg) {
   posX = msg->pose.pose.position.x;
   posY = msg->pose.pose.position.y;
+}
+
+void MecanumController::ColorImgCb(const sensor_msgs::ImagePtr& msg) {
+  cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
+  cv::Mat background;
+  // cv::cvtColor(img->image, background, cv::COLOR_RGB2HSV);
+  cv::cvtColor(img->image, background, cv::COLOR_RGB2Lab);
+  // cv::blur(background, background, cv::Size(5, 5), cv::Point(-1, -1));
+  cv::GaussianBlur(background, background, cv::Size(5, 5), 1.0, 4.0);
+  cv::Mat mask_orange;
+  // cv::inRange(background,
+  //             cv::Scalar(H_MIN_ORANGE, S_MIN_ORANGE, V_MIN_ORANGE),
+  //             cv::Scalar(H_MAX_ORANGE, S_MAX_ORANGE, V_MAX_ORANGE),
+  //             mask_orange);
+  cv::inRange(background,
+              cv::Scalar(L_MIN_ORANGE, A_MIN_ORANGE, B_MIN_ORANGE),
+              cv::Scalar(L_MAX_ORANGE, A_MAX_ORANGE, B_MAX_ORANGE),
+              mask_orange);
+  cv::dilate(mask_orange, mask_orange,
+             cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5), cv::Point(-1,-1)),
+             cv::Point(-1, -1), 3);
+  // cv::dilate(mask_orange, mask_orange,
+  //            cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5), cv::Point(-1,-1)),
+  //            cv::Point(-1, -1), 1);
+  // cv::blur(mask_orange, mask_orange, cv::Size(5, 5), cv::Point(-1, -1));
+  // cv::dilate(mask_orange, mask_orange,
+  //            cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5), cv::Point(-1,-1)),
+  //            cv::Point(-1, -1), 1);
+  std::vector<std::vector<cv::Point>> orange_contours;
+  cv::findContours(mask_orange, orange_contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+  // cv::imshow("masko", mask_orange);
+  // cv::imshow("backg", background);
+  // cv::waitKey(1);
+  for (const auto& contour : orange_contours) {
+    const auto area = cv::contourArea(contour);
+    if (area < 5000 or area > 20000) { // TODO: this has to be calibrated
+      continue;
+    }
+    auto bbox = cv::boundingRect(contour);
+    cv::rectangle(img->image, cv::Point(bbox.x, bbox.y), cv::Point(bbox.x + bbox.width, bbox.y + bbox.height), 
+                  cv::Scalar(255, 165, 0), 2, cv::LINE_8, 0);
+    cv::putText(img->image, cv::format("Area: %f", area) , cv::Point(bbox.x, bbox.y), 
+                cv::FONT_HERSHEY_DUPLEX, 1.0, CV_RGB(255,255,255), 2);
+  }  
+
+  detPub.publish(img->toImageMsg());
 }
